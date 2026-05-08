150b29cde3915f2ebf903cae1b1a385c55b763dc
Lokeswaran.M <113868084+Lokeswaran-M@users.noreply.github.com>
2026-05-08 15:24:25 +0530

    QR

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -55,28 +55,3 @@ const Footer = () => {
 
 export default Footer;
 
-
-
-
-
-
-// import React from "react";
-
-// const Footer = () => {
-//   return (
-//     <footer className="bg-gray-900 text-white text-center py-1 text-base">
-//       <p>
-//         © {new Date().getFullYear()}{" "}
-//         <a
-//           href="#"
-//           className="text-white no-underline transition-colors duration-300 hover:text-pink-400"
-//         >
-//           Lokeswaran M
-//         </a>
-//         . All rights reserved.
-//       </p>
-//     </footer>
-//   );
-// };
-
-// export default Footer;
